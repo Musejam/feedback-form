c0f11f6dfe1b24b16ebe0ca794279edda9139495
Kapil <kapv89@gmail.com>
2012-09-06 12:38:46 +0530

    tarundeep's work

---
+++ application/controllers/forms.php
@@ -0,0 +1,44 @@
+<?php
+
+class Forms_Controller extends Base_Controller{
+	
+	public $restful=true;
+	
+	public function get_index()
+	{
+		return View::make('forms.index')->with('title','Recommandation Form'); 
+	}
+
+	public function post_success()
+	{
+		$validation = Form::validate(Input::all());
+		
+		if($validation->fails())
+		{
+			return Redirect::to_route('forms')->with_errors($validation)->with_input();
+		}
+		else
+		{
+			Form::create(array(
+				'ctype' => Input::get('radio'),
+				'name' => Input::get('name'),
+				'url' => Input::get('url'),
+				'sgenre' => Input::get('genre'),
+				'artist' => Input::get('artist'),
+				'fname' => Input::get('file'),
+				'country' => Input::get('country')
+				));
+		        
+			return Redirect::to_route('forms')->with('message','Recommandation Successfully Added');
+		}
+	}
+	
+	public function get_show(){
+		$per_page=5;
+		
+		$pg=Form::paginate($per_page);
+		
+		return View::make('forms.show')->with('title','Previous Recommandations')->with('show',$pg);
+	}
+}
+?>
---
+++ application/views/layouts/form.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>{{$title}}</title>
+{{HTML::script('js/jquery.js')}}
+{{HTML::script('js/form.js')}}
+{{HTML::style('css/style.css')}}
+</head>
+<body>
+	@if(Session::has('message'))
+	<p style="color:green;">{{Session::get('message')}}</p>
+	@endif
+	@yield('content')
+	
+</body>
+</html>
